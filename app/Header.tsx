efb062b7503ae4e6ac04756c9666347c348fe2f4
Nikola Ćurčić <62150474+CureDoktor@users.noreply.github.com>
2024-01-15 23:53:30 +0100

    header update

--- app/Header.tsx
+++ app/Header.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Bars3Icon } from "@heroicons/react/24/solid";
 import Link from "next/link";
 import NavLinks from "./NavLinks";
 import SearchBox from "./SearchBox";
@@ -8,7 +7,6 @@ function Header() {
   return (
     <header>
       <div className="grid grid-cols-3 p-10 items-center">
-        <Bars3Icon className="h-8 w-8 cursor-pointer" />
         <Link href="/" prefetch={false}>
           <h1 className="font-serif text-4xl text-center">
             {" "}
